d368d08c9e617ca0e7a5ad3a0a228325852bf0c2
João Victor Vieira de Andrade <joaovictorvieira.23@hotmail.com>
2022-05-17 16:47:50 -0300

    feat: initial project

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Column(
+      children: <Widget>[
+        Text('Display'),
+        Text('Keyboard'),
+      ],
+    ));
+  }
+}

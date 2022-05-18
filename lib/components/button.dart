fb05e019797f05f33462f62aa96a7eceaed9fd8a
João Victor Vieira de Andrade <joaovictorvieira.23@hotmail.com>
2022-05-18 15:02:37 -0300

    feat: create function onPressed

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -8,23 +8,27 @@ class Button extends StatelessWidget {
   final String text;
   final bool big;
   final Color color;
+  final void Function(String) cb;
 
   Button({
     required this.text,
     this.big = false,
     this.color = DEFAULT,
+    required this.cb,
   });
 
   Button.big({
     required this.text,
     this.big = true,
     this.color = DEFAULT,
+    required this.cb,
   });
 
   Button.operation({
     required this.text,
     this.big = false,
     this.color = OPERATION,
+    required this.cb,
   });
 
   @override
@@ -38,7 +42,7 @@ class Button extends StatelessWidget {
                   fontSize: 32,
                   fontWeight: FontWeight.w200,
                   color: Colors.white)),
-          onPressed: () {},
+          onPressed: () => cb(text),
         ));
   }
 }

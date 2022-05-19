3a20dd2b8d677ab83ef583c84e371607a1c9bbca
Joao Andrade <joaovictorvieira.23@hotmail.com>
2022-05-19 15:08:26 -0500

    feat: set digit and display logic

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,12 +1,40 @@
 class Memory {
+    static const operations = const ['%', '/', 'x', '–', '+', '='];
+
     String _value = '0';
+    final _buffer = [0.0, 0.0];
+    int _bufferIndex = 0;
+    late String operation;
+    bool _wipeValue = false;
 
     void applyCommand(String command) {
       if(command == 'AC') {
-        _allClear();
-      } else {
-      _value += command;
+        _allClear(memory);
+      } else if (operations.contains(command)) {
+        _setOperation(command);
       }
+      else {
+        _addDigit(command);
+      }
+    }
+
+    _setOperation(String newOperation) {
+      _wipeValue = true;
+    }
+    
+    _addDigit(String digit) {
+      final isDot = digit == '.';
+      final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+      if(isDot && _value.contains('.') && !wipeValue) {
+        return;
+      }
+      final emptyValue = isDot ? '0' : '';
+      final currentValue = wipeValue ? emptyValue : _value;
+      _value = currentValue + digit;
+      _wipeValue = false;
+
+      _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
     }
 
     _allClear() {
